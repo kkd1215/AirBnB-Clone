2e109aaa067535a28ca9be07902cee7d521d7a4d
Karan Dewani <karandewani98@gmail.com>
2020-12-07 17:04:31 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return(
+    <div className='footer'>
+    <p>© 2020 Airbnb Clone! No rights reserved - this is a demo!</p>
+    <p><a href='#'>Privacy</a> · <a href='#'>Terms</a> · <a href='#'>Sitemap</a> · <a href='#'>Company Details</a></p>
+    </div>
+  )
+}
+
+export default Footer;
